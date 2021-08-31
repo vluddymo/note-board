86490cdc3a7cf5cbcd851fabfe38d565ff9d5256
vluddymo <wladi.moser@gmail.com>
2021-08-31 21:16:16 +0200

    Init Spring Backend

---
+++ note_board/src/main/java/de/vluddymo/note_board/NoteBoardApplication.java
@@ -0,0 +1,13 @@
+package de.vluddymo.note_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteBoardApplication.class, args);
+    }
+
+}
---
+++ note_board/src/test/java/de/vluddymo/note_board/NoteBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package de.vluddymo.note_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
