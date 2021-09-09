bd99933f01c7830331f2e65bc1d21b3e0b377466
vluddymo <wladi.moser@gmail.com>
2021-09-09 12:51:22 +0200

    Add document annotation to Note model

--- note_board/src/main/java/de/vluddymo/note_board/model/Note.java
+++ note_board/src/main/java/de/vluddymo/note_board/model/Note.java
@@ -3,10 +3,12 @@ package de.vluddymo.note_board.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Document(collection = "notes")
 public class Note {
 
     public String id;
