db1656901828226303fa32238b6e4729d4b44965
vluddymo <wladi.moser@gmail.com>
2021-09-09 12:48:16 +0200

    Inject service with getAllNotes function into controller

--- note_board/src/main/java/de/vluddymo/note_board/controller/NoteController.java
+++ note_board/src/main/java/de/vluddymo/note_board/controller/NoteController.java
@@ -1,6 +1,8 @@
 package de.vluddymo.note_board.controller;
 
 import de.vluddymo.note_board.model.Note;
+import de.vluddymo.note_board.service.NoteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +13,15 @@ import java.util.List;
 @RequestMapping("api/notes")
 public class NoteController {
 
+    private final NoteService noteService;
+
+    @Autowired
+    public NoteController(NoteService noteService){
+        this.noteService = noteService;
+    }
+
     @GetMapping
-    public List<Note> getNotes (){
-        return List.of(
-                new Note("1", "first Note"),
-                new Note("2", "second Note")
-        );
+    public Iterable<Note> getNotes (){
+       return noteService.getAllNotes();
     }
 }
